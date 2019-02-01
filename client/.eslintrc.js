a3f67f3e6bf041b675be14bb0fa7187895289e30
Matías Olivera <matiasolivera@outlook.com>
2019-02-01 18:10:01 -0300

    Integrar ESlint con Prettier
    https://prettier.io/docs/en/eslint.html

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
     '@vue/typescript',
+    'plugin:prettier/recommended'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: '@typescript-eslint/parser',
-  },
+    parser: '@typescript-eslint/parser'
+  }
 };
