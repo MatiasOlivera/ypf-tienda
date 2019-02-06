ff5b07fb9d5a0ef47128ae3bc2a1c2aa62b9bb46
Matías Olivera <matiasolivera@outlook.com>
2019-02-06 12:14:58 -0300

    Eliminar los componentes y las vistas de ejemplo

---
+++ client/src/views/Inicio.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Página de inicio</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Inicio'
+});
+</script>
