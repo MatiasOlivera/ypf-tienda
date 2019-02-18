6e5bbd8e169e2372754f9c4ee4dc51257c1bde0e
Matías Olivera <matiasolivera@outlook.com>
2019-02-18 14:55:22 -0300

    Añadir los tipos para vue-feather

---
+++ client/src/types/vue-feather.d.ts
@@ -0,0 +1,7 @@
+declare module 'vue-feather' {
+  // eslint-disable-next-line
+  import { PluginFunction } from 'vue/types/plugin';
+
+  const VueFeather: PluginFunction<any>;
+  export = VueFeather;
+}
