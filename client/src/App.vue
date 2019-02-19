6454a17ec2406622cb70b2ccae491e7bf5f5e31b
Matías Olivera <matiasolivera@outlook.com>
2019-02-18 21:25:52 -0300

    Agregar el componente PieDePagina

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,16 +2,31 @@
   <div id="app">
     <LaCabecera />
 
-    <RouterView />
+    <RouterView id="contenido" />
+
+    <ElPieDePagina />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import LaCabecera from './components/LaCabecera.vue';
+import ElPieDePagina from './components/ElPieDePagina.vue';
 
 export default Vue.extend({
   name: 'App',
-  components: { LaCabecera }
+  components: { LaCabecera, ElPieDePagina }
 });
 </script>
+
+<style lang="scss" scoped>
+#app {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+#contenido {
+  flex: 1;
+}
+</style>
---
+++ client/src/components/ElPieDePagina.vue
@@ -0,0 +1,81 @@
+<template>
+  <footer class="bg-light border-top mt-5">
+    <b-container>
+      <b-row class="mb-5">
+        <b-col class="d-flex justify-content-center">
+          <img id="logotipo" src="../assets/logotipo.png" alt="Logotipo" />
+        </b-col>
+      </b-row>
+
+      <b-row class="text-center">
+        <b-col cols="12" md="3">
+          <p>{{ empresa.razonSocial }}<br />{{ empresa.cuit | formatoCuit }}</p>
+        </b-col>
+
+        <b-col cols="12" md="3">
+          <address>
+            <ul class="list-unstyled">
+              <li v-for="(telefono, indice) in empresa.telefonos" :key="indice">
+                <a :href="`tel:+${telefono}`">
+                  {{ telefono | formatoTelefono }}
+                </a>
+              </li>
+            </ul>
+          </address>
+        </b-col>
+
+        <b-col cols="12" md="3">
+          <address>
+            <a :href="`mailto:${empresa.email}`">
+              {{ empresa.email }}
+            </a>
+          </address>
+        </b-col>
+
+        <b-col cols="12" md="3">
+          <p>
+            {{ empresa.direccion.calle }} <br />
+            {{ empresa.direccion.localidad }}, {{ empresa.direccion.provincia }}
+          </p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </footer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import empresa from '../config/empresa';
+import { formatoCuitMixin, formatoTelefonoMixin } from '../mixins';
+
+export default Vue.extend({
+  name: 'ElPieDePagina',
+  mixins: [formatoCuitMixin, formatoTelefonoMixin],
+  data: () => {
+    return { empresa };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+$color-fuente: rgba(0, 0, 0, 0.5);
+
+footer {
+  color: $color-fuente;
+  padding: 40px 0;
+}
+
+a {
+  color: $color-fuente;
+}
+
+#logotipo {
+  height: 40px;
+  filter: grayscale(100%) opacity(0.5);
+  transition: all 0.5s ease;
+}
+
+#logotipo:hover {
+  filter: grayscale(0%) opacity(1);
+}
+</style>
