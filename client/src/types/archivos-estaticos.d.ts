200e6d79d1c569f10eadf80f06de60f63aaf0fb9
Matías Olivera <matiasolivera@outlook.com>
2019-02-22 10:17:14 -0300

    Agregar los productos y servicios
    Definir los tipos de Typescript para poder importar archivos que no sean Javascript

---
+++ client/src/types/archivos-estaticos.d.ts
@@ -0,0 +1,14 @@
+declare module '*.png' {
+  const contenido: any;
+  export = contenido;
+}
+
+declare module '*.jpg' {
+  const contenido: any;
+  export = contenido;
+}
+
+declare module '*.jpeg' {
+  const contenido: any;
+  export = contenido;
+}
--- client/src/views/InicioView.vue
+++ client/src/views/InicioView.vue
@@ -41,6 +41,49 @@
           </p>
         </b-col>
       </b-row>
+
+      <b-row class="py-5">
+        <b-col class="text-center">
+          <h2 class="titulo text-uppercase">
+            Productos y servicios
+          </h2>
+        </b-col>
+      </b-row>
+
+      <b-row class="pb-1">
+        <b-col
+          v-for="(categoria, indice) in categorias"
+          :key="indice"
+          cols="12"
+          md="6"
+          xl="4"
+        >
+          <b-card
+            :title="categoria.nombre"
+            title-tag="p"
+            :img-src="categoria.rutaImagen"
+            :img-alt="categoria.nombre"
+            img-top
+            align="center"
+            class="tarjeta-categoria mb-4"
+            :class="{
+              'd-none d-sm-block': indice >= 3 && !verTodasLasCategorias
+            }"
+          />
+        </b-col>
+      </b-row>
+
+      <b-row class="pb-5">
+        <b-col class="d-flex justify-content-center d-block d-sm-none">
+          <b-button
+            v-if="!verTodasLasCategorias"
+            variant="outline-secondary"
+            @click="mostrarCategorias"
+          >
+            Ver más
+          </b-button>
+        </b-col>
+      </b-row>
     </b-container>
   </main>
 </template>
@@ -48,12 +91,87 @@
 <script lang="ts">
 import Vue from 'vue';
 
+import asesoramiento from '../static/images/categorias/asesoramiento-tecnico.jpg';
+import muestreoDeSuelo from '../static/images/categorias/muestreo-suelo.jpg';
+import canjeDeGranos from '../static/images/categorias/canje-de-granos.jpg';
+import lubricantes from '../static/images/categorias/lubricantes.jpg';
+import combustibles from '../static/images/categorias/combustibles.jpg';
+import grasas from '../static/images/categorias/grasas.jpg';
+import fitosanitarios from '../static/images/categorias/fitosanitarios.jpg';
+import fertilizantes from '../static/images/categorias/fertilizantes.jpg';
+import plasticos from '../static/images/categorias/plasticos.jpg';
+import servicioCapilar from '../static/images/categorias/servicio-capilar.jpg';
+
 export default Vue.extend({
-  name: 'InicioView'
+  name: 'InicioView',
+  data: (): Data => {
+    return {
+      categorias: [
+        {
+          nombre: 'Asesoramiento técnico',
+          rutaImagen: asesoramiento
+        },
+        {
+          nombre: 'Muestreo de suelo',
+          rutaImagen: muestreoDeSuelo
+        },
+        {
+          nombre: 'Canje de granos',
+          rutaImagen: canjeDeGranos
+        },
+        {
+          nombre: 'Lubricantes y especialidades',
+          rutaImagen: lubricantes
+        },
+        {
+          nombre: 'Combustibles',
+          rutaImagen: combustibles
+        },
+        {
+          nombre: 'Grasas',
+          rutaImagen: grasas
+        },
+        {
+          nombre: 'Fitosanitarios',
+          rutaImagen: fitosanitarios
+        },
+        {
+          nombre: 'Fertilizantes',
+          rutaImagen: fertilizantes
+        },
+        {
+          nombre: 'Plásticos',
+          rutaImagen: plasticos
+        },
+        {
+          nombre: 'Servicio capilar',
+          rutaImagen: servicioCapilar
+        }
+      ],
+      verTodasLasCategorias: false
+    };
+  },
+  methods: {
+    mostrarCategorias(): void {
+      this.verTodasLasCategorias = true;
+    }
+  }
 });
+
+interface Data {
+  verTodasLasCategorias: boolean;
+  categorias: Array<Categoria>;
+}
+
+interface Categoria {
+  nombre: string;
+  rutaImagen: string;
+}
 </script>
 
 <style lang="scss" scoped>
+@import '../static/styles/personalizado.scss';
+
 #hero {
   position: relative;
   height: 90vh;
@@ -77,5 +195,16 @@ export default Vue.extend({
   bottom: 0;
   width: 100%;
   height: 10vw;
+}
+
+.tarjeta-categoria {
+  transition: all ease 0.5s;
+  top: 0;
+}
+
+.tarjeta-categoria:hover {
+  // Clase shadow de Bootstrap
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15) !important;
+  top: -10px;
 }
 </style>
