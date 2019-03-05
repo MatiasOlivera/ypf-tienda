7a44a6873221fa2e29621bf3fb87eacd51ba92e2
CIrel Romeo <cirelhromeo@gmail.com>
2019-03-05 20:35:37 -0300

    FIX: nombre migración  tabla cliente_usuarios

--- server/database/migrations/2014_10_12_000000_create_cliente_usuarios_table.php
+++ server/database/migrations/2014_10_12_000000_create_cliente_usuarios_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateClienteUsuarioTable extends Migration
+class CreateClienteUsuariosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateClienteUsuarioTable extends Migration
      */
     public function up()
     {
-        Schema::create('cliente_usuario', function (Blueprint $table) {
+        Schema::create('cliente_usuarios', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
@@ -36,7 +36,7 @@ class CreateClienteUsuarioTable extends Migration
     public function down()
     {
         Schema::disableForeignKeyConstraints();
-        Schema::dropIfExists('cliente_usuario');
+        Schema::dropIfExists('cliente_usuarios');
         Schema::enableForeignKeyConstraints();
     }
 }
