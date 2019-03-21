d9b510c11b1518b3fe4d5bcc1803a53afc4c51d7
CIrel Romeo <cirelhromeo@gmail.com>
2019-03-21 02:45:22 -0300

    ADD migration: procedure para copiar datos de con_cliente a cliente_mail

---
+++ server/database/migrations/2019_03_20_162053_copiar_mails_a_cliente_mails.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CopiarMailsAClienteMails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared("CREATE DEFINER=`root`@`localhost` PROCEDURE `mover_mails`()
+                SQL SECURITY INVOKER
+            BEGIN
+                DECLARE fin         INT DEFAULT FALSE;
+                DECLARE contacto_id INT(11);
+                DECLARE cliente_id  INT(11);
+                DECLARE email       varchar(250);
+                DECLARE creado      DATETIME;
+                DECLARE eliminado   DATETIME;
+                DECLARE mails  CURSOR FOR SELECT id, id_cliente, mail, created_at, deleted_at FROM con_cliente WHERE mail IS NOT NULL;
+                DECLARE CONTINUE HANDLER FOR NOT FOUND SET fin = TRUE;
+
+            OPEN mails;
+                copiar_mails: LOOP
+
+                        FETCH mails INTO contacto_id, cliente_id, email, creado, eliminado;
+
+                        IF fin THEN
+                            LEAVE copiar_mails;
+                        END IF;
+
+                        INSERT INTO cliente_mails (contacto_id, cliente_id, email, created_at, deleted_at)
+                                            VALUES (contacto_id, cliente_id, email, creado,     eliminado);
+
+                    END LOOP copiar_mails;
+                CLOSE mails;
+            END;
+
+            call `mover_mails`();
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared("TRUNCATE TABLE cliente_mails;
+                        DROP PROCEDURE IF EXISTS mover_mails;");
+    }
+}
