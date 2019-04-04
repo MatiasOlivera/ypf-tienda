18710dd963bebb850ba653cdde5e37dfbd434ef3
CIrel Romeo <cirelhromeo@gmail.com>
2019-04-04 18:00:54 -0300

    ADD: agrega las relaciones en cliente

--- server/app/Cliente.php
+++ server/app/Cliente.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     protected $table = 'clientes';
+    protected $primaryKey = 'id_cliente';
 
     /**
     * The attributes that should be mutated to dates.
@@ -21,8 +22,28 @@ class Cliente extends Model
      */
     protected $fillable = ['dni', 'cliente', 'obsevacion', 'otros', ];
 
-    public function user()
+    public function users()
     {
         return $this->hasMany('App\User', 'id_cliente', 'id_cliente');
+    }
+
+    public function domicilios()
+    {
+        return $this->hasMany('App\ClienteDomicilio', 'id_cliente', 'id_cliente')->with(['localidad', ]);
+    }
+
+    public function telefonos()
+    {
+        return $this->hasMany('App\ClienteTelefono', 'id_cliente', 'id_cliente')->whereNotNull('area');
+    }
+
+    public function mails()
+    {
+        return $this->hasMany('App\ClienteMail', 'id_cliente', 'id_cliente')->whereNotNull('mail');
+    }
+
+    public function razonesSociales()
+    {
+        return $this->belongsToMany('App\ClienteRazonSocial', 'cliente_razon', 'id_cliente', 'id_razon')->with(['localidad', ])->withTimestamps();
     }
 }
