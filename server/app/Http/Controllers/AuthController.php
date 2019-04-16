61b08ee760dd3269f5428b2f3afbe60618616b7e
Matías Olivera <matiasolivera@outlook.com>
2019-04-16 18:41:34 -0300

    feat: usar la clase respuesta y mensajes en la autenticación

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Auxiliares\{Respuesta, MensajeExito, MensajeError};
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -9,7 +10,6 @@ use JWTAuth;
 use Hash;
 use tymon\JWTAuth\Exceptions\JWTException;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Exceptions\HttpResponseException;
 use Carbon\Carbon;
 
 //Users Request
@@ -17,21 +17,18 @@ use App\Http\Requests\UsersRequest\UserLoginRequest;
 
 class AuthController extends Controller
 {
-
     public function login(UserLoginRequest $request)
     {
         $credenciales = $request->only('email', 'password');
 
         try {
             if (!$token = JWTAuth::attempt($credenciales)) {
-                $mensaje['error'] = 'Credenciales invalidas';
-                $status = 401;
-                return response()->json($mensaje, $status);
+                $mensajeError = new MensajeError('Credenciales inválidas', 'CREDENCIALES_INVALIDAS');
+                return Respuesta::error($mensajeError, 401);
             }
         } catch (JWTException $e) {
-            $mensaje['error'] = 'no pudimos autenticarlo';
-            $status = 500;
-            return response()->json($mensaje, $status);
+            $mensajeError = new MensajeError('Hubo un problema al intentar iniciar la sesión', 'NO_AUTENTICADO');
+            return Respuesta::error($mensajeError, 500);
         }
 
         return $this->setRespuestaToken($token);
@@ -47,15 +44,11 @@ class AuthController extends Controller
         try {
             auth()->logout();
 
-            $mensaje = 'Sesión cerrada';
-            $status = 200;
-
-            return response()->json(compact('mensaje'), $status);
+            $mensaje = new MensajeExito('Sesión cerrada', 'LOGOUT');
+            return Respuesta::exito([], $mensaje, 200);
         } catch (\Throwable $th) {
-            $mensaje = 'Tuvimos un problema';
-            $status = 500;
-
-            return response()->json(compact('mensaje'), $status);
+            $mensaje = new MensajeError('Hubo un problema al intentar cerrar la sesión', 'NO_LOGOUT');
+            return Respuesta::error($mensaje, 200);
         }
     }
 
@@ -67,7 +60,8 @@ class AuthController extends Controller
 
     public function usuario(Request $request)
     {
-        return $request->user();
+        $usuario = $request->user();
+        return Respuesta::exito(['usuario' => $usuario], null, 200);
     }
 
     /**
@@ -78,12 +72,11 @@ class AuthController extends Controller
     public function renovar(Request $request)
     {
         try {
-            $token = $this->guard()->refresh();
+            $token = Auth::guard()->refresh();
             return $this->setRespuestaToken($token);
         } catch (JWTException $e) {
-            $mensaje = 'Token Invalido';
-            $status = 401;
-            return response()->json(compact('mensaje'), $status);
+            $mensaje = new MensajeError('Token inválido', 'NO_VALIDO');
+            return Respuesta::error($mensaje, 401);
         }
     }
 
@@ -94,21 +87,18 @@ class AuthController extends Controller
      */
     protected function setRespuestaToken($token)
     {
-        $ttl = $this->guard()->factory()->getTTL();
+        $ttl = Auth::guard()->factory()->getTTL();
 
         $fechaActual = Carbon::now();
         $fechaExpiracion = $fechaActual->addMinutes($ttl);
         $fechaExpiracionJSON = $fechaExpiracion->toIso8601String();
 
-        return response()->json([
+        $autenticacion = [
             'token' => $token,
             'tipoToken' => 'bearer',
             'fechaExpiracion' => $fechaExpiracionJSON
-        ]);
-    }
+        ];
 
-    public function guard()
-    {
-        return Auth::guard();
+        return Respuesta::exito(['autenticacion' => $autenticacion], null, 200);
     }
 }
