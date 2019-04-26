b69ccb984ab71cbdf40c9d9fc64c92572f3d7c11
Matías Olivera <matiasolivera@outlook.com>
2019-04-26 20:46:10 -0300

    style: corregir case en migraciones

--- server/database/migrations/2019_03_20_144056_alter_tabla_con_clientes_add_time_stamp_soft_delete.php
+++ server/database/migrations/2019_03_20_144056_alter_tabla_con_clientes_add_time_stamp_soft_delete.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTablaConClientesAddTimeStampSOftDelete extends Migration
+class AlterTablaConClientesAddTimeStampSoftDelete extends Migration
 {
     /**
      * Run the migrations.
