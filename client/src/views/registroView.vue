023c81eba49fa1ad339567edf1bf8ef0e9da5b33
CIrel Romeo <cirelhromeo@gmail.com>
2019-05-13 09:12:40 -0300

    Add: registroView component

---
+++ client/src/views/registroView.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <h2>Registro</h2>
+    <div>
+      <ClienteFormulario></ClienteFormulario>
+    </div>
+  </main>
+</template>
+<script>
+import ClienteFormulario from '../components/ClienteFormulario.vue';
+
+export default {
+  name: 'RegistroView',
+  components: { ClienteFormulario },
+  data() {
+    return {
+      datos: null
+    };
+  },
+  methods: {
+    cancelarRegistro() {
+      this.$emit('cancelarRegistro');
+    }
+  }
+};
+</script>
+<style>
+.clase {
+  color: aliceblue;
+}
+</style>
