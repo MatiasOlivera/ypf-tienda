84ce2cf1514134cee24f895fccaa8fb26c16aefb
CIrel Romeo <cirelhromeo@gmail.com>
2019-05-13 09:14:42 -0300

    ADD: clienteFormulario componente

---
+++ client/src/components/ClienteFormulario.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>Cliente</h3>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      formulario: {
+        nombre: null,
+        documento: null,
+        observacion: null
+      }
+    };
+  }
+};
+</script>
