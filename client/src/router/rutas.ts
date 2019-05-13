0e624271cc3185e4896585d99ae38af72a29234e
CIrel Romeo <cirelhromeo@gmail.com>
2019-05-13 09:14:04 -0300

    Add: ruta registro en rutas.ts

--- client/src/router/rutas.ts
+++ client/src/router/rutas.ts
@@ -4,12 +4,18 @@ import InicioView from '../views/InicioView.vue';
 
 // Nombres
 export const rutaInicio = 'inicio';
+export const rutaRegistro = 'registro';
 
 export const rutas: Array<RouteConfig> = [
   {
     path: '/',
     name: rutaInicio,
     component: InicioView
+  },
+  {
+    path: '/registro',
+    name: rutaRegistro,
+    component: () => import('../views/registroView.vue')
   }
 ];
 
