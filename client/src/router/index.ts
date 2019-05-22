437324a6e122e5ee24831b8ec95801fe912ef868
Matías Olivera <matiasolivera@outlook.com>
2019-05-22 16:50:24 -0300

    fix(cliente): separar únicamente el nombre de las rutas
    Me salía un error de dependencia ciclica, cuando se importaba el nombre de una ruta desde un componente que estaba lista en rutas.ts

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,12 +1,19 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import { rutas } from './rutas';
+import InicioView from '../views/InicioView.vue';
+import { rutaInicio } from './rutas';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: rutas
+  routes: [
+    {
+      path: '/',
+      name: rutaInicio,
+      component: InicioView
+    }
+  ]
 });
--- client/src/router/rutas.ts
+++ client/src/router/rutas.ts
@@ -1,16 +1,1 @@
-import { RouteConfig } from 'vue-router';
-
-import InicioView from '../views/InicioView.vue';
-
-// Nombres
 export const rutaInicio = 'inicio';
-
-export const rutas: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: rutaInicio,
-    component: InicioView
-  }
-];
-
-export default rutas;
