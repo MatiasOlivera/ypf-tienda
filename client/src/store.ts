c80361e0fa2ad3e78040b2f2ccedb536a04ff468
CIrel Romeo <cirelhromeo@gmail.com>
2019-05-24 16:16:29 -0300

    ADD: agrerga el modulo Cliente al store

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import modules from './store/modulos/index';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  modules,
   state: {},
   mutations: {},
   actions: {}
