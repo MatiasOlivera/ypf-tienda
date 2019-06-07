e0b0d7203320f18671d5d7c227c247ca0dcbe227
Matías Olivera <matiasolivera@outlook.com>
2019-06-07 19:20:01 -0300

    feat(cliente): agregar el módulo autenticación

--- client/src/store/types/mutaciones.ts
+++ client/src/store/types/mutaciones.ts
@@ -3,3 +3,9 @@
  */
 export const AGREGAR_NOTIFICACION = 'agregarNotificacion';
 export const ELIMINAR_ULTIMA_NOTIFICACION = 'eliminarUltimaNotificacion';
+
+/**
+ * Autenticación
+ */
+export const SET_USUARIO = 'setUsuario';
+export const SET_ESTA_LOGUEADO = 'setEstaLogueado';
