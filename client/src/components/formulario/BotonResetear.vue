2ef0de885cd7431dc34e466037dc731f671de986
Matías Olivera <matiasolivera@outlook.com>
2019-06-07 20:01:48 -0300

    feat(cliente): agregar botones para usar en los formularios

---
+++ client/src/components/formulario/BotonResetear.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-button type="reset" variant="link" class="text-muted">
+    Resetear
+  </b-button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'BotonResetear'
+});
+</script>
---
+++ client/src/components/formulario/BotonSubmit.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-button :disabled="cargando" type="submit" variant="outline-primary">
+    <template v-if="cargando">
+      <b-spinner small variant="primary" label="Cargando"></b-spinner>
+      Cargando
+    </template>
+
+    <template v-else>
+      {{ texto }}
+    </template>
+  </b-button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { PropValidator } from 'vue/types/options';
+
+export type SubmitPropCargando = boolean;
+export type SubmitPropTexto = 'Guardar' | 'Actualizar';
+const texto = ['Guardar', 'Actualizar'];
+
+export default Vue.extend({
+  name: 'BotonSubmit',
+
+  props: {
+    cargando: {
+      type: Boolean,
+      default: false
+    } as PropValidator<SubmitPropCargando>,
+
+    texto: {
+      type: String,
+      default: 'Guardar',
+      validator: (valor) => texto.includes(valor)
+    } as PropValidator<SubmitPropTexto>
+  }
+});
+</script>
