49cc7a33f69e3a4388c13b50aaf8d287949d5f1d
Matías Olivera <matiasolivera@outlook.com>
2019-06-10 21:41:58 -0300

    feat(cliente): utilizar el componente LaCabecera en App

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <LaCabecera />
+    <LaCabecera
+      :esta-logueado="estaLogueado"
+      :nombre-usuario="nombreUsuario"
+      @click-logotipo="irAInicio"
+      @click-login="irAIniciarSesion"
+      @click-cerrar-sesion="cerrarSesion"
+    />
 
     <RouterView id="contenido" />
 
@@ -11,21 +17,37 @@
 <script lang="ts">
 /* eslint-disable no-prototype-builtins */
 import Vue from 'vue';
-import { mapGetters } from 'vuex';
+import { mapState, mapGetters, mapActions } from 'vuex';
 
+// Componentes
 import LaCabecera from './components/LaCabecera.vue';
 import ElPieDePagina from './components/ElPieDePagina.vue';
-import { MODULO_NOTIFICACIONES } from './store/types/modulos';
-import { Notificacion } from './types/tipos-notificacion';
-import { mostrarNotificacion } from './utils/notificaciones';
+
+// Store
+import {
+  MODULO_AUTENTICACION,
+  MODULO_NOTIFICACIONES
+} from './store/types/modulos';
+import { LOGOUT } from './store/types/acciones';
+import { NOMBRE_USUARIO, ULTIMA_NOTIFICACION } from './store/types/getters';
 import { UltimaNotificacion } from './store/modules/notificaciones/modulo-notificaciones';
-import { ULTIMA_NOTIFICACION } from './store/types/getters';
+
+// Tipos
+import { Notificacion } from './types/tipos-notificacion';
+
+// Router
+import { rutaInicio, rutaLogin } from './router/rutas';
+
+// Otros
+import { mostrarNotificacion } from './utils/notificaciones';
 
 export default Vue.extend({
   name: 'App',
   components: { LaCabecera, ElPieDePagina },
 
   computed: {
+    ...mapState(MODULO_AUTENTICACION, ['estaLogueado']),
+    ...mapGetters(MODULO_AUTENTICACION, [NOMBRE_USUARIO]),
     ...mapGetters(MODULO_NOTIFICACIONES, [ULTIMA_NOTIFICACION])
   },
 
@@ -38,6 +60,24 @@ export default Vue.extend({
         }
       },
       deep: true
+    }
+  },
+
+  methods: {
+    ...mapActions(MODULO_AUTENTICACION, [LOGOUT]),
+
+    irAInicio(): void {
+      this.$router.push({ name: rutaInicio });
+    },
+
+    irAIniciarSesion(): void {
+      this.$router.push({ name: rutaLogin });
+    },
+
+    cerrarSesion(): void {
+      this.logout().then(() => {
+        this.$router.push({ name: rutaInicio });
+      });
     }
   }
 });
