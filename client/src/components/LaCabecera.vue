eba8feedca2940b8ecb236a0eae495c39fd59bc4
Matías Olivera <matiasolivera@outlook.com>
2019-06-10 21:36:15 -0300

    feat(cliente): agregar datos de autenticación en la cabecera

--- client/src/components/LaCabecera.vue
+++ client/src/components/LaCabecera.vue
@@ -7,7 +7,7 @@
     class="shadow"
   >
     <b-container>
-      <b-navbar-brand :to="{ name: rutaInicio }">
+      <b-navbar-brand @click="clickLogotipo">
         <img src="../static/images/logotipo.png" alt="Logotipo" />
       </b-navbar-brand>
 
@@ -25,13 +25,24 @@
         </b-navbar-nav>
 
         <b-navbar-nav class="ml-auto">
-          <b-button size="sm" variant="link">
-            Iniciar sesión
-          </b-button>
+          <b-nav-item-dropdown v-if="estaLogueado" :text="nombreUsuario" right>
+            <b-dropdown-item @click="clickPerfil">
+              Perfil
+            </b-dropdown-item>
+            <b-dropdown-item @click="clickCerrarSesion">
+              Cerrar sesión
+            </b-dropdown-item>
+          </b-nav-item-dropdown>
 
-          <b-button size="sm" variant="link">
-            Registrarse
-          </b-button>
+          <template v-else>
+            <b-button size="sm" variant="link" @click="clickLogin">
+              Iniciar sesión
+            </b-button>
+
+            <b-button size="sm" variant="link" @click="clickRegistro">
+              Registrarse
+            </b-button>
+          </template>
         </b-navbar-nav>
       </b-collapse>
     </b-container>
@@ -40,14 +51,48 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import { rutaInicio } from '../router/rutas';
+import { PropValidator } from 'vue/types/options';
+import { rutaInicio, rutaLogin } from '../router/rutas';
+
+// Props
+export type PropEstaLogueado = boolean;
+export type PropNombreUsuario = string;
 
 export default Vue.extend({
   name: 'LaCabecera',
-  data: () => {
-    return {
-      rutaInicio
-    };
+
+  props: {
+    estaLogueado: {
+      type: Boolean,
+      default: false
+    } as PropValidator<PropEstaLogueado>,
+
+    nombreUsuario: {
+      type: String,
+      default: ''
+    } as PropValidator<PropNombreUsuario>
+  },
+
+  methods: {
+    clickLogotipo(): void {
+      this.$emit('click-logotipo');
+    },
+
+    clickLogin(): void {
+      this.$emit('click-login');
+    },
+
+    clickRegistro(): void {
+      this.$emit('click-registro');
+    },
+
+    clickPerfil(): void {
+      this.$emit('click-perfil');
+    },
+
+    clickCerrarSesion(): void {
+      this.$emit('click-cerrar-sesion');
+    }
   }
 });
 </script>
