42609a571c6208bc9eaf742b8760afa72cdc15a1
Matías Olivera <matiasolivera@outlook.com>
2019-06-27 18:28:18 -0300

    feat(servidor): agregar el modelo categoria producto

---
+++ server/app/CategoriaProducto.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Sofa\Eloquence\{Eloquence, Mappable};
+
+class CategoriaProducto extends Model
+{
+    use SoftDeletes, Eloquence, Mappable;
+
+    protected $table = 'categorias';
+    protected $primaryKey = 'ID_CAT_prod';
+
+    protected $fillable = ['descripcion'];
+
+    protected $maps = [
+        'id' => 'ID_CAT_prod',
+        'descripcion' => 'desc_cat'
+    ];
+
+    protected $appends = ['id', 'descripcion'];
+
+    protected $visibble = ['id', 'descripcion'];
+
+    protected $hidden = ['ID_CAT_prod', 'desc_cat', 'estado'];
+
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+}
