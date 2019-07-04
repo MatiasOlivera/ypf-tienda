9eed00f2da246dbf2dc092c03c6aa28276272827
Matías Olivera <matiasolivera@outlook.com>
2019-07-04 16:27:03 -0300

    fix(servidor): devolver las provincias que han sido eliminadas
    Soluciona el error "No query results for model [App\Provincia]"

--- server/app/Providers/RouteServiceProvider.php
+++ server/app/Providers/RouteServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\User;
 use App\Cliente;
 use App\Producto;
+use App\Provincia;
 use App\ClienteMail;
 use App\ClienteTelefono;
 use App\ClienteDomicilio;
@@ -88,6 +89,13 @@ class RouteServiceProvider extends ServiceProvider
         Route::bind('producto', function ($id) {
             return Producto::withTrashed()
                 ->where('id', $id)
+                ->firstOrFail();
+        });
+
+        // Provincia
+        Route::bind('provincia', function ($id) {
+            return Provincia::withTrashed()
+                ->where('id_provincia', $id)
                 ->firstOrFail();
         });
     }
