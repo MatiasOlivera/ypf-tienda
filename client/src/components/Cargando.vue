5a13b3ddbf6afe9cf55ce985b4128234e097bf42
Matías Olivera <matiasolivera@outlook.com>
2019-07-15 20:04:29 -0300

    feat(cliente): agregar el componente cargando

---
+++ client/src/components/Cargando.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="contenedor">
+    <loading :active.sync="estaCargando" :is-full-page="pantallaCompleta"></loading>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+// Componentes
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+
+// Props
+export type PropEstaCargando = boolean;
+export type PropPantallaCompleta = boolean;
+
+export default Vue.extend({
+  name: 'Cargando',
+
+  components: { Loading },
+
+  props: {
+    estaCargando: {
+      type: Boolean,
+      default: false
+    } as PropValidator<PropEstaCargando>,
+
+    pantallaCompleta: {
+      type: Boolean,
+      default: false
+    } as PropValidator<PropPantallaCompleta>
+  }
+});
+</script>
+
+<style scoped>
+#contenedor {
+  position: relative;
+}
+</style>
