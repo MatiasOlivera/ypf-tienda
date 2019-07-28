bc256fc9508e35c9df4d5547975c6403c6ff8913
Matías Olivera <matiasolivera@outlook.com>
2019-07-28 19:10:49 -0300

    test(servidor): crear un teléfono

--- server/tests/Feature/app/Http/Controllers/ClienteTelefonoControllerTest.php
+++ server/tests/Feature/app/Http/Controllers/ClienteTelefonoControllerTest.php
@@ -13,12 +13,31 @@ use Tests\Feature\Utilidades\EstructuraJsonHelper;
 class ClienteTelefonoControllerTest extends TestCase
 {
     use AuthHelper;
+    use WithFaker;
     use RefreshDatabase;
     use EstructuraJsonHelper;
+
+    private $estructuraTelefono = [
+        'telefono' => [
+            'id',
+            'area',
+            'telefono',
+            'nombreContacto',
+            'cliente_id',
+            'created_at',
+            'updated_at',
+            'deleted_at'
+        ]
+    ];
 
     private function getEstructuraTelefonos()
     {
         return array_merge(['telefonos'], $this->estructuraPaginacion);
+    }
+
+    private function getEstructuraTelefono()
+    {
+        return array_merge($this->estructuraTelefono, $this->estructuraMensaje);
     }
 
     /**
@@ -64,5 +83,73 @@ class ClienteTelefonoControllerTest extends TestCase
             ->assertOk()
             ->assertJsonStructure($estructura)
             ->assertJson(['telefonos' => $telefonos]);
+    }
+
+    /**
+     * Debería crear un teléfono con nombre de contacto
+     */
+    public function testDeberiaCrearUnTelefonoConNombre()
+    {
+        $telefono = factory(ClienteTelefono::class, 1)->make([
+            'nombreContacto' => $this->faker->firstName()
+        ])->toArray()[0];
+        $id = $telefono['cliente_id'];
+
+        $cabeceras = $this->loguearseComo('defecto');
+        $respuesta = $this
+            ->withHeaders($cabeceras)
+            ->json('POST', "api/clientes/$id/telefonos", $telefono);
+
+        $estructura = $this->getEstructuraTelefono();
+        unset($telefono['id']);
+
+        $respuesta
+            ->assertStatus(201)
+            ->assertJsonStructure($estructura)
+            ->assertJson([
+                'telefono' => $telefono,
+                'mensaje' => [
+                    'tipo' => 'exito',
+                    'codigo' => 'GUARDADO',
+                    'descripcion' => "El teléfono de {$telefono['nombreContacto']} ha sido creado"
+                ]
+            ]);
+
+        $nombreContacto = $respuesta->getData(true)['telefono']['nombreContacto'];
+        $this->assertEquals($telefono['nombreContacto'], $nombreContacto);
+    }
+
+     /**
+     * Debería crear un teléfono sin nombre de contacto
+     */
+    public function testDeberiaCrearUnTelefonoSinNombre()
+    {
+        $telefono = factory(ClienteTelefono::class, 1)->make()->toArray()[0];
+        $id = $telefono['cliente_id'];
+
+        unset($telefono['nombreContacto']);
+
+        $cabeceras = $this->loguearseComo('defecto');
+        $respuesta = $this
+            ->withHeaders($cabeceras)
+            ->json('POST', "api/clientes/$id/telefonos", $telefono);
+
+        $estructura = $this->getEstructuraTelefono();
+        unset($telefono['id']);
+
+        $respuesta
+            ->assertStatus(201)
+            ->assertJsonStructure($estructura)
+            ->assertJson([
+                'telefono' => $telefono,
+                'mensaje' => [
+                    'tipo' => 'exito',
+                    'codigo' => 'GUARDADO',
+                    'descripcion' => "El teléfono {$telefono['area']}-{$telefono['telefono']} ha sido creado"
+                ]
+            ]);
+
+        $nombreContacto = $respuesta->getData(true)['telefono']['nombreContacto'];
+        $this->assertNull($nombreContacto);
     }
 }
