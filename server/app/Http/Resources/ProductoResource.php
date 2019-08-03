6ced5092336ff497eb822bda5edf2f1a09a420ed
Matías Olivera <matiasolivera@outlook.com>
2019-08-03 17:09:04 -0300

    feat(servidor): formatear las fechas al devolver un JSON
    https://laravel.com/docs/5.8/eloquent-serialization#date-serialization

--- server/app/Http/Resources/ProductoResource.php
+++ server/app/Http/Resources/ProductoResource.php
@@ -30,9 +30,9 @@ class ProductoResource extends JsonResource
             // usuario logueado es un cliente
             'es_favorito' => $this->esFavorito,
             'id_categoria' => $this->id_categoria,
-            'created_at' => (string) $this->created_at,
-            'updated_at' => (string) $this->updated_at,
-            'deleted_at' => $this->deleted_at ? (string) $this->deleted_at : null
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at
         ];
     }
 }
