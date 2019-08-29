c3938c18672315cb3ea995b9101a9ecaa2e31f7d
Matías Olivera <matiasolivera@outlook.com>
2019-08-28 21:11:16 -0300

    feat(servidor): agregar la relación cotizaciones en cliente domicilio

---
+++ server/tests/Feature/app/ClienteDomicilioTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\app;
+
+use App\Cotizacion;
+use Tests\TestCase;
+use App\ClienteDomicilio;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ClienteDomicilioTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_deberia_acceder_a_la_relacion_cotizaciones()
+    {
+        $domicilio = factory(ClienteDomicilio::class)->create();
+
+        $nuevaCotizacion = factory(Cotizacion::class)->make([
+            'domicilio_id' => $domicilio->id
+        ]);
+
+        $domicilio->cotizaciones()->save($nuevaCotizacion);
+
+        $cotizacion = $domicilio->cotizaciones()->first();
+
+        $this->assertInstanceOf(Cotizacion::class, $cotizacion);
+        $this->assertEquals($cotizacion->domicilio_id, $domicilio->id);
+    }
+}
