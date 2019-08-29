063d593c32982dd52e3bc0e75cc129b0fcae2131
Matías Olivera <matiasolivera@outlook.com>
2019-08-28 22:07:43 -0300

    feat(servidor): agregar el modelo cotizacion

---
+++ server/database/factories/CotizacionFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Cliente;
+use App\Empleado;
+use App\Cotizacion;
+use App\Observacion;
+use App\ClienteTelefono;
+use App\ClienteDomicilio;
+use App\CotizacionEstado;
+use App\ClienteRazonSocial;
+use Faker\Generator as Faker;
+
+$factory->define(Cotizacion::class, function (Faker $faker) {
+    return [
+        'fecha_pedido' => $faker->date(),
+        'consumidor_final' => $faker->randomElement(['0', '1']),
+        'plazo' => $faker->word(),
+        'estado_id' => function () {
+            return factory(CotizacionEstado::class)->create()->id;
+        },
+        'observacion_id' => function () {
+            return factory(Observacion::class)->create()->id;
+        },
+
+        /**
+         * Empleado
+         */
+        'empleado_id' => function () {
+            return factory(Empleado::class)->create()->id;
+        },
+
+        /**
+         * Cliente
+         */
+        'cliente_id' => function () {
+            return factory(Cliente::class)->create()->id;
+        },
+        'razon_id' => function () {
+            return factory(ClienteRazonSocial::class)->create()->id;
+        },
+        'telefono_id' => function () {
+            return factory(ClienteTelefono::class)->create()->id;
+        },
+        'domicilio_id' => function () {
+            return factory(ClienteDomicilio::class)->create()->id;
+        }
+    ];
+});
---
+++ server/tests/Feature/Utilidades/EstructuraCotizacion.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Utilidades;
+
+trait EstructuraCotizacion
+{
+    private $atributosCotizacion = [
+        'id',
+        'empleado_id',
+        'cliente_id',
+        'razon_id',
+        'fecha_pedido',
+        'estado_id',
+        'consumidor_final',
+        'plazo',
+        'telefono_id',
+        'domicilio_id',
+        'pedido_id',
+        'observacion_id',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
